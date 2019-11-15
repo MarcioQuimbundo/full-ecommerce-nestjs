1c571020b4cc14628fac3177fa84bc22ba65116e
MarcioQuimbundo <marcioquimbundo@hotmail.com>
2019-11-15 20:08:40 +0100

    http-exception filter added and shared modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/types/order.ts
+++ src/types/order.ts
@@ -1,4 +1,5 @@
 import { Document } from 'mongoose';
+
 import { Product } from './product';
 import { User } from './user';
 
