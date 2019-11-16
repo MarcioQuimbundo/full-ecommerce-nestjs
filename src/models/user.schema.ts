47bd7bb80fb2a3f8fec4fa229e482aa88dcbd096
MarcioQuimbundo <marcioquimbundo@hotmail.com>
2019-11-16 20:34:30 +0100

    Defines a pre hook for the document UserSchema.

--- src/models/user.schema.ts
+++ src/models/user.schema.ts
@@ -1,4 +1,5 @@
 import * as mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 export const UserSchema = new mongoose.Schema({
     name: String,
@@ -18,5 +19,18 @@ export const UserSchema = new mongoose.Schema({
     created: {
         type: Date,  
         default: Date.now,
+    }
+});
+
+UserSchema.pre("save", async function(next: mongoose.HookNextFunction){
+    try {
+        if(!this.isModified('password')) {
+            return next();
+        }
+        const hashed = await bcrypt.hash(this['password'], 10);
+        this['password'] = hashed;
+        return next();
+    } catch(err) {
+        return next(err);
     }
 });
