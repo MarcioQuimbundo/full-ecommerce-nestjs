1870b54a33c536020802b2550c77bb3990987c03
MarcioQuimbundo <marcioquimbundo@hotmail.com>
2019-11-18 12:11:41 +0100

    TasksController added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
+import { TasksController } from './tasks/tasks.controller';
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.MONGO_URI), SharedModule, AuthModule],
-  controllers: [AppController],
+  controllers: [AppController, TasksController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
